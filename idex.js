d89baffa0f14117b30f3766e6213c1f0685dcc50
vj_akash <48998005+vjakash@users.noreply.github.com>
2020-05-14 18:39:14 +0530

    first commit

---
+++ idex.js
@@ -0,0 +1,99 @@
+var playList = /** @class */ (function () {
+    function playList(name) {
+        this.playListSongs = [];
+        this.playListName = name;
+    }
+    playList.prototype.addSong = function (son) {
+        // console.log(son);
+        this.playListSongs.push(son);
+    };
+    return playList;
+}());
+var tamil = new playList("Tamil");
+var english = new playList("English");
+var songs = /** @class */ (function () {
+    function songs() {
+    }
+    return songs;
+}());
+var artists = /** @class */ (function () {
+    function artists(name) {
+        this.songs = [];
+        this.artistName = name;
+    }
+    return artists;
+}());
+var musicDirector = /** @class */ (function () {
+    function musicDirector(name) {
+        this.songs = [];
+        this.artistName = name;
+    }
+    return musicDirector;
+}());
+var album = /** @class */ (function () {
+    function album() {
+    }
+    return album;
+}());
+var artist;
+function add() {
+    var song = new songs();
+    song.songName = document.getElementById("songName").value;
+    song.artist = document.getElementById("artistName").value;
+    artist = new artists(song.artist);
+    song.genre = document.getElementById("genre").value;
+    song.musicDirector = (document.getElementById("musicDirectorName")).value;
+    song.url = document.getElementById("url").value;
+    song.album = document.getElementById("albumName").value;
+    switch (document.getElementById("playlist").value) {
+        case "Tamil":
+            tamil.addSong(song);
+            break;
+        case "English":
+            english.addSong(song);
+            break;
+    }
+    document.getElementById("songName").value = "";
+    document.getElementById("artistName").value = "";
+    document.getElementById("genre").value = "";
+    document.getElementById("musicDirectorName").value = "";
+    document.getElementById("url").value = "";
+    document.getElementById("albumName").value = "";
+    updatePlayList();
+}
+function updatePlayList() {
+    var displayDiv = document.getElementById("listedSongs");
+    switch (document.getElementById("list").value) {
+        case "Tamil":
+            displayDiv.innerHTML = "";
+            if (tamil.playListSongs.length == 0) {
+                displayDiv.innerHTML = " No Songs in the list";
+                return;
+            }
+            tamil.playListSongs.forEach(function (item) {
+                var arg = JSON.stringify(item);
+                var div1 = document.createElement("div");
+                div1.setAttribute("class", "row");
+                div1.innerHTML += "&emsp;<div class='col-lg-10 text-left text-capitalize'>" + item.songName + "</div>\n                            <div class='col-lg-2 text-left'><button class='btn btn-dark' onclick='play(" + arg + ")' style=\"height:2vw;\"><span class=\"material-icons\">\n                            play_circle_outline\n                            </span></button></div> ";
+                displayDiv.appendChild(div1);
+            });
+            break;
+        case "English":
+            displayDiv.innerHTML = "";
+            if (english.playListSongs.length == 0) {
+                displayDiv.innerHTML = " No Songs in the list";
+                return;
+            }
+            english.playListSongs.forEach(function (item) {
+                var arg = JSON.stringify(item);
+                displayDiv.innerHTML += "<div class=\"row \" >&emsp;<div class='col-8 col-lg-8 text-left text-capitalize'>" + item.songName + "</div>\n                            <div class= 'col-3 col-lg-3 text-left'><button class='btn btn-light' onclick='play(" + arg + ")' style=\"height:2.5em;\"><span class=\"material-icons\" style=\"font-size:2em;\">\n                            play_circle_outline\n                            </span></button></div> </div><br>";
+            });
+            break;
+    }
+}
+function play(obj) {
+    // obj=JSON.parse(obj);
+    console.log(obj);
+    document.getElementById("player").innerHTML = "";
+    document.getElementById("player").innerHTML = "<div class=\"row\">\n    <div class=\"col-lg-12 bg-white border border-white\">\n        <br>\n        <div class=\"row \">\n            <div class=\"col-1 col-lg-3\"></div>\n            <div class=\"col-6 col-lg-6\">\n                <img src=\"record.png\" style=\"height:20rem;width:20rem\" id=\"record\" />\n            </div>\n            <div class=\" col-lg-3\"></div>\n            <br>\n        </div>\n        <br>\n        <div class=\"row bg-dark text-white\">\n            <div class=\"col-lg-12 text-center\">\n                <h1 class=\"text-capitalize text-center\" style=\"font-size:5vw;\">" + obj.songName + "</h1>\n                <p class=\"text-capitalize\">Artists: " + obj.artist + "&emsp;&emsp;Gengre: " + obj.genre + "&emsp;&emsp;Music Director: " + obj.musicDirector + "</p>\n                <h3 class=\"text-capitalize\">Album: " + obj.album + "</h3>\n            </div>\n        </div>\n        <div class=\"row bg-dark\">\n            <div class=\"col-lg-12\">\n                <br>\n                <audio controls autoplay style=\"width: 100%;\">\n                    <source src=" + obj.url + " type=\"audio/ogg\">\n                    <source src=" + obj.url + " type=\"audio/mpeg\">\n                    Your browser does not support the audio element.\n                  </audio>\n                <br><br>\n            </div>\n        </div>\n    </div>\n</div>";
+}
---
+++ idex.ts
@@ -0,0 +1,146 @@
+type playListType = "Tamil" | "English";
+
+class playList {
+  playListName: playListType;
+  playListSongs: songs[] = [];
+  constructor(name) {
+    this.playListName = name;
+  }
+
+  addSong(son: songs) {
+    // console.log(son);
+    this.playListSongs.push(son);
+  }
+}
+let tamil = new playList("Tamil");
+let english = new playList("English");
+class songs {
+  songName: String;
+  artist;
+  album;
+  url;
+  musicDirector;
+  genre;
+}
+
+class artists {
+  artistName: String;
+  songs: songs[] = [];
+  constructor(name) {
+    this.artistName = name;
+  }
+}
+class musicDirector {
+    artistName: String;
+    songs: songs[] = [];
+    constructor(name) {
+      this.artistName = name;
+    }
+  }
+class album {
+  albumName;
+  songs;
+}
+
+let artist;
+function add() {
+  let song = new songs();
+  song.songName = (<HTMLInputElement>document.getElementById("songName")).value;
+  song.artist = (<HTMLInputElement>document.getElementById("artistName")).value;
+  artist = new artists(song.artist);
+  song.genre = (<HTMLInputElement>document.getElementById("genre")).value;
+  song.musicDirector = (<HTMLInputElement>(
+    document.getElementById("musicDirectorName")
+  )).value;
+  song.url = (<HTMLInputElement>document.getElementById("url")).value;
+  song.album = (<HTMLInputElement>document.getElementById("albumName")).value;
+  switch ((<HTMLInputElement>document.getElementById("playlist")).value) {
+    case "Tamil":
+      tamil.addSong(song);
+      break;
+    case "English":
+      english.addSong(song);
+      break;
+  }
+  (<HTMLInputElement>document.getElementById("songName")).value = "";
+  (<HTMLInputElement>document.getElementById("artistName")).value = "";
+  (<HTMLInputElement>document.getElementById("genre")).value = "";
+  (<HTMLInputElement>document.getElementById("musicDirectorName")).value = "";
+  (<HTMLInputElement>document.getElementById("url")).value = "";
+  (<HTMLInputElement>document.getElementById("albumName")).value = "";
+  updatePlayList();
+}
+function updatePlayList() {
+  let displayDiv = <HTMLElement>document.getElementById("listedSongs");
+  switch ((<HTMLSelectElement>document.getElementById("list")).value) {
+    case "Tamil":
+      displayDiv.innerHTML = "";
+      if(tamil.playListSongs.length==0){
+        displayDiv.innerHTML = " No Songs in the list";
+        return;
+      }
+      tamil.playListSongs.forEach((item) => {
+          let arg=JSON.stringify(item);
+        let div1 = document.createElement("div");
+        div1.setAttribute("class", "row");
+        div1.innerHTML += `&emsp;<div class='col-lg-10 text-left text-capitalize'>${item.songName}</div>
+                            <div class='col-lg-2 text-left'><button class='btn btn-dark' onclick='play(${arg})' style="height:2vw;"><span class="material-icons">
+                            play_circle_outline
+                            </span></button></div> `;
+        displayDiv.appendChild(div1);
+      });
+      break;
+    case "English":
+      displayDiv.innerHTML = "";
+      if(english.playListSongs.length==0){
+        displayDiv.innerHTML = " No Songs in the list";
+        return;
+      }
+      english.playListSongs.forEach((item) => {
+        let arg=JSON.stringify(item);
+
+        displayDiv.innerHTML += `<div class="row " >&emsp;<div class='col-8 col-lg-8 text-left text-capitalize'>${item.songName}</div>
+                            <div class= 'col-3 col-lg-3 text-left'><button class='btn btn-light' onclick='play(${arg})' style="height:2.5em;"><span class="material-icons" style="font-size:2em;">
+                            play_circle_outline
+                            </span></button></div> </div><br>`;
+      });
+      break;
+  }
+}
+function play(obj) {
+// obj=JSON.parse(obj);
+    console.log(obj);
+  (<HTMLElement>document.getElementById("player")).innerHTML = "";
+  (<HTMLElement>document.getElementById("player")).innerHTML = `<div class="row">
+    <div class="col-lg-12 bg-white border border-white">
+        <br>
+        <div class="row ">
+            <div class="col-1 col-lg-3"></div>
+            <div class="col-6 col-lg-6">
+                <img src="record.png" style="height:20rem;width:20rem" id="record" />
+            </div>
+            <div class=" col-lg-3"></div>
+            <br>
+        </div>
+        <br>
+        <div class="row bg-dark text-white">
+            <div class="col-lg-12 text-center">
+                <h1 class="text-capitalize text-center" style="font-size:5vw;">${obj.songName}</h1>
+                <p class="text-capitalize">Artists: ${obj.artist}&emsp;&emsp;Gengre: ${obj.genre}&emsp;&emsp;Music Director: ${obj.musicDirector}</p>
+                <h3 class="text-capitalize">Album: ${obj.album}</h3>
+            </div>
+        </div>
+        <div class="row bg-dark">
+            <div class="col-lg-12">
+                <br>
+                <audio controls autoplay style="width: 100%;">
+                    <source src=${obj.url} type="audio/ogg">
+                    <source src=${obj.url} type="audio/mpeg">
+                    Your browser does not support the audio element.
+                  </audio>
+                <br><br>
+            </div>
+        </div>
+    </div>
+</div>`;
+}
